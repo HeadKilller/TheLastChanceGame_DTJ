f4b7fc96d11fb2f12e3adfe4303129a7c459d5ea
Vitor200 <81098074+Vitor200@users.noreply.github.com>
2022-04-19 15:27:10 +0100

    ZombieImplementation / Bugs Fixed

---
+++ Assets/Scripts/ZombieBehavior.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieBehavior : MonoBehaviour
+{
+    [SerializeField] private int zombie_InitialHealth;
+    //[SerializeField] private Animator ZombieAnim;
+
+    private int zombie_CurrentHealth;
+
+    private void Start()
+    {
+        zombie_CurrentHealth = zombie_InitialHealth;
+    }
+
+    private void Update()
+    {
+
+    }
+
+
+    private void ZombieDeath()
+    {
+
+        //ZombieAnim.SetTrigger("Zombie_Death");
+        Destroy(gameObject);
+
+    }
+
+    public void ZombieHit(int dmg)
+    {
+
+        //ZombieAnim.SetTrigger("Zombie_Hit");
+
+
+        zombie_CurrentHealth -= dmg;
+
+        //Debug.Log("Zombie Health: " + zombie_CurrentHealth);
+
+        if (zombie_CurrentHealth <= 0) ZombieDeath();
+
+    }
+
+
+}
